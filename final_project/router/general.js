9fb2c83d51d48dc5c0d90e1efe46a8b8262b454d
CeeObi <chukwudimaobi@gmail.com>
2023-09-03 11:54:29 -0400

    updated GET all and specific

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,19 +13,24 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books));  
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    //Write your code here
+  let isbn = req.params.isbn;
+    let book = books[isbn];
+    res.send(JSON.stringify(book));
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let author = req.params.author;
+    //STILL TRYING TO SORT HERE OUT, TASK 3
+  let fbook= books.filter((book)=>{if (book.author===author){return book}})
+  res.send(JSON.stringify(fbook));
 });
 
 // Get all books based on title
