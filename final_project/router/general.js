903b3861f33d4985faf0112c8fbc6056799b82a6
CeeObi <chukwudimaobi@gmail.com>
2023-09-04 06:48:24 -0400

    completed GET routes for public_users

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -27,22 +27,48 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  let author = req.params.author;
-    //STILL TRYING TO SORT HERE OUT, TASK 3
-  let fbook= books.filter((book)=>{if (book.author===author){return book}})
-  res.send(JSON.stringify(fbook));
+  let author = JSON.stringify(req.params.author);  
+    //STILL TRYING TO SORT HERE OUT, TASK 3  
+    let found_book = []
+    Object.entries(books).forEach(([key,value]) => {
+        let authr =JSON.stringify(value['author'])            
+        if (author === authr){
+            found_book.push(books[key])            
+        }         
+    })
+    if (found_book.length > 0){
+            console.log("Yes")
+            res.send(JSON.stringify(found_book))}
+    else{
+    res.send({Error: "Book author is unavaiable"})}    
+ 
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+   //Write your code here
+   let title = JSON.stringify(req.params.title);     
+   let found_book = []
+   Object.entries(books).forEach(([key,value]) => {
+       let ttle =JSON.stringify(value['title'])            
+       if (title === ttle){
+        found_book.push(books[key])            
+       }         
+   })
+   if (found_book.length > 0){
+           console.log("Yes")
+           res.send(JSON.stringify(found_book))}
+   else{
+   res.send({Error: "Book title is unavaiable"})}    
+
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    //Write your code here
+    let isbn = req.params.isbn;
+    let book = books[isbn]['reviews'];
+    res.send(JSON.stringify(book));
+ });
 
 module.exports.general = public_users;
